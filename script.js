4ec3d0f9cc98cc5ea64f88c0d2f49f973059430a
henryhuangg <henryhuang.edu@gmail.com>
2023-07-22 10:19:46 -0400

    Royal Tech Webstore
    New Interface for the webstore:
    -Cleaner UI
    -Footer is styled and links to privacy policy, and contact links
    -About page is functional
    -index.html nav bar links should all work
    -Product grid containers need to be linked to their respective product category page.
    -products page needs to be styled and developed

--- script.js
+++ script.js
@@ -1,124 +1,124 @@
 async function parseProducts() {
-    const response = await fetch("./products.json");
-    const products = await response.json();
+  const response = await fetch("./products.json");
+  const products = await response.json();
 
-    products.product.forEach(function (data) {
-        var li = document.createElement("li");
-        var a = document.createElement("a");
-        a.href = "./product.html";
-        a.onclick = function () {
-            localStorage.setItem("name", data.name);
-            localStorage.setItem("price", data.price);
-            localStorage.setItem("img", data.images[0]);
-        };
-        a.innerHTML = data.name;
-        li.appendChild(a);
-        document.getElementById("paginated-list").appendChild(li);
-    });
+  products.product.forEach(function (data) {
+    var li = document.createElement("li");
+    var a = document.createElement("a");
+    a.href = "./product.html";
+    a.onclick = function () {
+      localStorage.setItem("name", data.name);
+      localStorage.setItem("price", data.price);
+      localStorage.setItem("img", data.images[0]);
+    };
+    a.innerHTML = data.name;
+    li.appendChild(a);
+    document.getElementById("paginated-list").appendChild(li);
+  });
 
-    paginateProducts();
+  paginateProducts();
 }
 
 function paginateProducts() {
-    const paginationNumbers = document.getElementById("pagination-numbers");
-    const paginatedList = document.getElementById("paginated-list");
-    const listItems = paginatedList.querySelectorAll("li");
-    const nextButton = document.getElementById("next-button");
-    const prevButton = document.getElementById("prev-button");
+  const paginationNumbers = document.getElementById("pagination-numbers");
+  const paginatedList = document.getElementById("paginated-list");
+  const listItems = paginatedList.querySelectorAll("li");
+  const nextButton = document.getElementById("next-button");
+  const prevButton = document.getElementById("prev-button");
 
-    const paginationLimit = 5;
-    const pageCount = Math.ceil(listItems.length / paginationLimit);
-    let currentPage = 1;
+  const paginationLimit = 5;
+  const pageCount = Math.ceil(listItems.length / paginationLimit);
+  let currentPage = 1;
 
-    const disableButton = (button) => {
-        button.classList.add("disabled");
-        button.setAttribute("disabled", true);
-    };
+  const disableButton = (button) => {
+    button.classList.add("disabled");
+    button.setAttribute("disabled", true);
+  };
 
-    const enableButton = (button) => {
-        button.classList.remove("disabled");
-        button.removeAttribute("disabled");
-    };
+  const enableButton = (button) => {
+    button.classList.remove("disabled");
+    button.removeAttribute("disabled");
+  };
 
-    const handlePageButtonsStatus = () => {
-        if (currentPage === 1) {
-            disableButton(prevButton);
-        } else {
-            enableButton(prevButton);
-        }
+  const handlePageButtonsStatus = () => {
+    if (currentPage === 1) {
+      disableButton(prevButton);
+    } else {
+      enableButton(prevButton);
+    }
 
-        if (pageCount === currentPage) {
-            disableButton(nextButton);
-        } else {
-            enableButton(nextButton);
-        }
-    };
+    if (pageCount === currentPage) {
+      disableButton(nextButton);
+    } else {
+      enableButton(nextButton);
+    }
+  };
 
-    const handleActivePageNumber = () => {
-        document.querySelectorAll(".pagination-number").forEach((button) => {
-            button.classList.remove("active");
-            const pageIndex = Number(button.getAttribute("page-index"));
-            if (pageIndex == currentPage) {
-                button.classList.add("active");
-            }
-        });
-    };
-
-    const appendPageNumber = (index) => {
-        const pageNumber = document.createElement("button");
-        pageNumber.className = "pagination-number";
-        pageNumber.innerHTML = index;
-        pageNumber.setAttribute("page-index", index);
-        pageNumber.setAttribute("aria-label", "Page " + index);
-
-        paginationNumbers.appendChild(pageNumber);
-    };
-
-    const getPaginationNumbers = () => {
-        for (let i = 1; i <= pageCount; i++) {
-            appendPageNumber(i);
-        }
-    };
-
-    const setCurrentPage = (pageNum) => {
-        currentPage = pageNum;
-
-        handleActivePageNumber();
-        handlePageButtonsStatus();
-
-        const prevRange = (pageNum - 1) * paginationLimit;
-        const currRange = pageNum * paginationLimit;
-
-        listItems.forEach((item, index) => {
-            item.classList.add("hidden");
-            if (index >= prevRange && index < currRange) {
-                item.classList.remove("hidden");
-            }
-        });
-    };
-
-    window.addEventListener("load", () => {
-        getPaginationNumbers();
-        setCurrentPage(1);
-
-        prevButton.addEventListener("click", () => {
-            setCurrentPage(currentPage - 1);
-        });
-
-        nextButton.addEventListener("click", () => {
-            setCurrentPage(currentPage + 1);
-        });
-
-        document.querySelectorAll(".pagination-number").forEach((button) => {
-            const pageIndex = Number(button.getAttribute("page-index"));
-
-            if (pageIndex) {
-                button.addEventListener("click", () => {
-                    setCurrentPage(pageIndex);
-                });
-            }
-        });
+  const handleActivePageNumber = () => {
+    document.querySelectorAll(".pagination-number").forEach((button) => {
+      button.classList.remove("active");
+      const pageIndex = Number(button.getAttribute("page-index"));
+      if (pageIndex == currentPage) {
+        button.classList.add("active");
+      }
     });
+  };
+
+  const appendPageNumber = (index) => {
+    const pageNumber = document.createElement("button");
+    pageNumber.className = "pagination-number";
+    pageNumber.innerHTML = index;
+    pageNumber.setAttribute("page-index", index);
+    pageNumber.setAttribute("aria-label", "Page " + index);
+
+    paginationNumbers.appendChild(pageNumber);
+  };
+
+  const getPaginationNumbers = () => {
+    for (let i = 1; i <= pageCount; i++) {
+      appendPageNumber(i);
+    }
+  };
+
+  const setCurrentPage = (pageNum) => {
+    currentPage = pageNum;
+
+    handleActivePageNumber();
+    handlePageButtonsStatus();
+
+    const prevRange = (pageNum - 1) * paginationLimit;
+    const currRange = pageNum * paginationLimit;
+
+    listItems.forEach((item, index) => {
+      item.classList.add("hidden");
+      if (index >= prevRange && index < currRange) {
+        item.classList.remove("hidden");
+      }
+    });
+  };
+
+  window.addEventListener("load", () => {
+    getPaginationNumbers();
+    setCurrentPage(1);
+
+    prevButton.addEventListener("click", () => {
+      setCurrentPage(currentPage - 1);
+    });
+
+    nextButton.addEventListener("click", () => {
+      setCurrentPage(currentPage + 1);
+    });
+
+    document.querySelectorAll(".pagination-number").forEach((button) => {
+      const pageIndex = Number(button.getAttribute("page-index"));
+
+      if (pageIndex) {
+        button.addEventListener("click", () => {
+          setCurrentPage(pageIndex);
+        });
+      }
+    });
+  });
 }
 
 parseProducts();
